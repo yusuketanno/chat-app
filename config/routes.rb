d9140304f0dfce4b9bc6e2481ad258ac09dbf9fd
yusuketanno <tylf1007@gmail.com>
2020-10-02 09:41:51 +0900

    修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root "rooms#index"
-  resources :users, only: [:edit, :update]
-  resources :rooms, only: [:new, :create, :destroy] do
-    resources :messages, only: [:index, :create]
+  resources :users,    only: [:edit, :update]
+  resources :rooms,    only: [:new, :create, :destroy] do
+  resources :messages, only: [:index, :create]
   end
 end
